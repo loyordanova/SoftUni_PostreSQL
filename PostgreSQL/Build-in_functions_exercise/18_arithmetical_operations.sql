66e9f74747c55fb91064029f089a26da5b97499c
loyordanova <lora.yordanova@yahoo.com>
2024-04-15 22:54:47 +0300

    add more solutions

---
+++ PostgreSQL/Build-in_functions_exercise/18_arithmetical_operations.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bookings_calculation
+	AS
+SELECT
+	booked_for,
+	CAST(booked_for AS NUMERIC) * 50 AS multiplication,
+	CAST(booked_for AS NUMERIC) % 50 AS modulo
+	
+FROM bookings
+WHERE 
+	apartment_id = 93;
